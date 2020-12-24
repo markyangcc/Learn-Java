a7d818684f18c258fff7a572d410e66bacca7578
markyangcc <guangshengyang01@gmail.com>
2020-12-24 16:20:56 +0800

    remove .class files

--- Calendar/Calender.java
+++ Calendar/Calender.java
@@ -1,76 +1,90 @@
 import java.util.Scanner;
 
-public class Calender{
+public class Calendar {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("请输入年份");
-        int year=sc.nextInt();
-        System.out.println("请输入月份");
-        int month=sc.nextInt();
-        /*
-        1、计算当月总天数
-         */
-        int days;
-        if(month==2){
-            if(year%4==0&&year%100!=0||year%400==0){
-                days=29;
-            }else{
-                days=28;
-            }
-        }else if (month==4||month==6||month==9||month==11){
-            days=30;
-        }else{
-            days=31;
-        }
-        /*
-        2、计算当月1号距离1900年1月1号的天数
-         */
-        int sum=0;
-        //1.1对1900年到year之间的天数累加
-        for(int i=1900;i<year;i++){
-            if (i%4==0&&i%100!=0||i%400==0){
-                sum+=366;
-            }else {
-                sum+=365;
+        int allYearDays = 0;
+        int allMonthDays = 0;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("输入年份： ");
+        int nowYear = in.nextInt();
+
+        System.out.print("输入月份： ");
+        int nowMonth = in.nextInt();
+
+        System.out.print("输入每周从周几开始： ");
+        int weekStartOn = in.nextInt();
+        // int nowYear = 2020;
+        // int nowMonth = 12;
+        // int weekStartOn = 5;
+
+        // 计算当前年之前的年份的天数
+        for (int i = 1900; i < nowYear; i++) {
+            if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0)) {
+                allYearDays += 366;
+            } else {
+                allYearDays += 365;
             }
         }
-        //对月份天数累加
-        for(int i=1;i<month;i++){
-            if(i==2){
-                if(year%4==0&&year%100!=0||year%400==0){
-                    sum+=29;
-                }else{
-                    sum+=28;
+
+        // 计算当前年当前月之前的月份的天数
+        boolean isLeapYear = (nowYear % 4 == 0 && nowYear % 100 != 0) || (nowYear % 400 == 0);
+        for (int i = 1; i < nowMonth; i++) {
+            // 4,6,9,11月 30天
+            if (i == 4 || i == 6 || i == 9 || i == 11) {
+                allMonthDays += 30;
+            } else if (i == 2) {
+                // 2月闰年29天，平年28天
+                if (isLeapYear) {
+                    allMonthDays += 29;
+                } else {
+                    allMonthDays += 28;
                 }
-            }else if(i==4||i==6||i==9||i==11){
-                sum+=30;
-            }else{
-                sum+=31;
+            } else {
+                // 其他月份31天
+                allMonthDays += 31;
             }
         }
-        //对天数累加
-        sum+=1;
-        
-        /*
-        3.打印当月日历
-         */
-        System.out.println("------"+year+"年"+month+"月"+"的日历为："+"------");
-        
-        //计算当月1号是星期几，前面打印几个空格
-        System.out.println("日\t一\t二\t三\t四\t五\t六");
-        for(int i=0;i<sum%7;i++){
+
+        // 计算当前月天数
+        int thisMonthDays;
+        if (nowMonth == 4 || nowMonth == 6 || nowMonth == 9 || nowMonth == 11) {
+            thisMonthDays = 30;
+        } else if (nowMonth == 2) {
+            if (isLeapYear) {
+                thisMonthDays = 29;
+            } else {
+                thisMonthDays = 28;
+            }
+        } else {
+            thisMonthDays = 31;
+        }
+
+        // 计算当月1号对应星期几
+        int dayOneOfWeek = ((allYearDays + allMonthDays) % 7) + 1;
+        int spaceBeforeDayOne = ((7 - weekStartOn) + dayOneOfWeek) % 7;
+
+        System.out.println(nowYear + " 年 " + nowMonth + " 月 日历");
+
+        // 打印日历抬头周几
+        String[] weeks = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        for (int i = 0; i < weeks.length; i++) {
+            System.out.print(weeks[(weekStartOn - 1 + i) % 7] + "\t");
+        }
+        System.out.println();
+
+        // 打印每月前的空格(Weeks starts on Monday)
+        for (int i = 0; i < spaceBeforeDayOne; i++) {
             System.out.print("\t");
         }
 
-        //如果是星期六，换行
-        for(int i=1;i<=days;i++){
-            if(sum%7==6){
-                System.out.println(i);
-            }else{
-                System.out.print(i+"\t");
-            }
-            sum++;
+        // 打印日历
+        for (int i = 1; i <= thisMonthDays; i++) {
+            System.out.print(i + "\t");
+
+            if ((i + spaceBeforeDayOne) % 7 == 0)
+                System.out.println();
         }
     }
 }
-
