04696119a389edb9f11d26eaeb614f1ff1312656
markyangcc <guangshengyang01@gmail.com>
2020-12-28 13:00:32 +0800

    feat: add js code

---
+++ Java Web/4- var.js
@@ -0,0 +1,51 @@
+/*
+* js 变量类型
+*
+*
+* js 特殊的值
+* undefined  未初始化，默认值全部为undefined
+* null        空值
+* NAN         Not a number
+*
+*
+* 等于 和 全等于
+* == 等于只做简单的字面值比较
+* === 全等于还要做类型比较
+*
+*
+* 逻辑运算
+*
+* && and
+* || or
+* | 取反
+* 前提条件，在js中，所有变量都可以当作bool类型使用
+* 0,null，undefined，"" 认为是false
+*
+* and 运算
+* 1、表达式全真时，返回最后一个表达式的值
+* 2、当表达式为假时，返回第一个为假的表达式的值
+*
+* or 运算
+* 1、表达式全假，返回最后一个表达式的值
+* 2、只有一个表达式为真，就会返回第一个为真的值
+*
+* js数组 重点
+* var var_name = [];
+*
+*
+* fun 有隐藏参数，
+*
+*
+* js中的Object自定义对象,就是对象 OOP
+* object
+* var name = new Object();
+*name.attribute = ;
+*
+* 花括号型的自定义对象
+* var objname = {
+*                 attribute1:value;
+*                 attribute2:value;
+*                 attribute3:value;
+*                };
+*
+* */
