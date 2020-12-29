8e10c808943b38ffd2226b78e90a41ab653ec518
markyangcc <guangshengyang01@gmail.com>
2020-12-29 19:59:45 +0800

    update on master

--- Java Web/4- var.js
+++ Java Web/4- var.js
@@ -43,9 +43,33 @@
 *
 * 花括号型的自定义对象
 * var objname = {
-*                 attribute1:value;
-*                 attribute2:value;
-*                 attribute3:value;
+*                 attribute1:value,
+*                 attribute2:value,
+*                 attribute3:value,
+*                 function_name: function(){};
 *                };
+*
+*
+* js中的事件
+* 电脑与界面进行交互，叫事件
+*
+* 常用事件：
+* onload 加载完成事件     页面加载完成后，常用于页面js初始化操作
+* onclick                常用于按钮点击操作
+* onblur                 常用于输入框失去焦点后验证输入内容是否合法
+* onchange                 常用于下拉列表和输入框内容发生改变后操作
+* onsubmit                 常用于表单提交前，验证所有表单项时候合法
+*
+* 事件的注册（绑定）：告诉浏览器，当时间响应后要执行那些操作代码，就叫事件的注册/绑定
+*
+* 静态注册
+* 通过html标签的事件属性直接赋予事件响应后的代码
+*
+* 动态注册
+*先通过js代码得到标签的dom对象，然后再通过dom对象。事件名 = function（）{}这种形式赋予响应后的代码，叫动态注册
+*
+* 动态注册基本步骤：
+*   1、获取标签对象
+*   2、function(){ }
 *
 * */
